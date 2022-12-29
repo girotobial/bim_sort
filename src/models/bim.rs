ffad66bb673246945980f106d7935618c4f74694
girotobial <girotobial@gmail.com>
2022-12-29 12:05:07 +0000

    add read write functions to bimfile

--- src/models/bim.rs
+++ src/models/bim.rs
@@ -1,12 +1,31 @@
+pub use io::Result;
+use std::fs;
+use std::io;
+use std::path::PathBuf;
+
 use serde::{Deserialize, Serialize};
 
 use super::prelude::Model;
 
 #[derive(Serialize, Deserialize, Debug, PartialEq)]
 #[serde(rename_all = "camelCase")]
-pub struct BimFile {
+pub struct Bim {
     pub name: String,
     pub compatibility_level: u32,
     pub model: Model,
     pub id: String,
+}
+
+impl Bim {
+    pub fn from_file(path: &PathBuf) -> io::Result<Self> {
+        let data = fs::read_to_string(path)?;
+        let res: Bim = serde_json::from_str(&data)?;
+        Ok(res)
+    }
+
+    pub fn to_file(self: &Self, path: &PathBuf) -> io::Result<()> {
+        let contents = serde_json::to_string_pretty(self)?;
+        fs::write(path, contents)?;
+        Ok(())
+    }
 }
--- src/models/prelude.rs
+++ src/models/prelude.rs
@@ -1,4 +1,4 @@
-pub use super::bim::BimFile;
+pub use super::bim::Bim;
 pub use super::datasource::DataSource;
 pub use super::expression::{Expression, Expressive};
 pub use super::model::Model;
